508f1fead4645bd4579fe10702269e2cf272058f
bj-rn <schwarzer@posteo.net>
2024-07-15 15:22:51 +0200

    PdfMatches Items now IReadOnlyList

--- src/PdfMatches.cs
+++ src/PdfMatches.cs
@@ -13,7 +13,7 @@ namespace VL.PDFReader
 
         public int EndPage { get; private set; }
 
-        public IList<PdfMatch> Items { get; private set; }
+        public IReadOnlyList<PdfMatch> Items { get; private set; }
 
         public PdfMatches(int startPage, int endPage, IList<PdfMatch> matches)
         {
